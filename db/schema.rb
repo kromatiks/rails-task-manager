f8204fd14575b23572a6bfbc1b9c811f2711b76f
Ningjie Cao <kromatiks1@gmail.com>
2024-03-06 15:57:06 +0800

    index

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_06_072041) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_06_073902) do
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
     t.text "details"
     t.boolean "completed", default: false
     t.datetime "created_at", null: false
